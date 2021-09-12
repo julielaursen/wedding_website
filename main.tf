e036c9aeafcb09c7d1d9d847ceb55439131dab59
julielaursen <julielaursen1@gmail.com>
2021-09-12 16:27:19 -0500

    add info in aws provider area

--- main.tf
+++ main.tf
@@ -18,10 +18,12 @@
 #}
 
 provider "aws" {
+  source = "hashicorp/aws"
   region = var.aws_region
   #aws_access_key_id = var.aws_access_key_id
   #aws_secret_key = var.aws_secret_key
 }
+
 
 provider "cloudflare" {
   #Cloudflare email saved in $CLOUDFLARE_EMAIL
