1e6ea7baa97708fb9d30cb85a352663189d51490
julielaursen <julielaursen1@gmail.com>
2021-09-12 15:08:57 -0500

    Added var in main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 provider "cloudflare" {
   #Cloudflare email saved in $CLOUDFLARE_EMAIL
   #Cloudflare api token saved in $CLOUDFLARE_API_TOKEN
+  CLOUDFLARE_API_TOKEN=var.CLOUDFLARE_API_TOKEN
 }
 
 resource "aws_s3_bucket" "site" {
