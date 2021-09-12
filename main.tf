08561396e1ef977149df90eed4dff043e2bbf2dc
julielaursen <julielaursen1@gmail.com>
2021-09-12 15:24:41 -0500

    remove vars from main tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = var.aws_region
-  aws_access_key_id = var.aws_access_key_id
-  aws_secret_key = var.aws_secret_key
+  #aws_access_key_id = var.aws_access_key_id
+  #aws_secret_key = var.aws_secret_key
 }
 
 provider "cloudflare" {
